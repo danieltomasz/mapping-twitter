823b8ecb18d104a36e484640af8615323339ecf0
mjboos <moritz.boos@uni-oldenburg.de>
2019-03-03 10:43:56 +0100

    preprocessing with twitterscraper

--- code/download_twitter_followers.py
+++ code/download_twitter_followers.py
@@ -1,12 +1,24 @@
 import tweepy
 import numpy as np
 import json
+import joblib
 
 # adapted from https://stackoverflow.com/a/42390494
 
 def get_id_from_user_screennames(user_list, api):
     '''Returns list of ids for list of user screen names'''
     return [api.get_user(user).id for user in user_list]
+
+def download_followers_from_id(user, api):
+    all_followers = []
+    try:
+        for page in tweepy.Cursor(api.followers_ids, id=user).pages():
+            all_followers.extend(page)
+#            all_followers.extend([api.get_user(user_id).screen_name for user_id in page])
+        return all_followers
+    except tweepy.TweepError:
+        print('Could not access user {}. Skipping...'.format(user))
+
 
 def download_followers(user, api):
     all_followers = []
@@ -19,9 +31,16 @@ def download_followers(user, api):
         print('Could not access user {}. Skipping...'.format(user))
 
 
-def get_screen_names_of_followers(user, api):
+def scrape_tweets_from_user(username, limit=1000):
+    '''scrapes tweets and saves them in ../data'''
+    import twitterscraper
+    import os
+    os.system('twitterscraper {0} -u -l {1} -bd 2016-01-01 -ed 2019-03-03 -o ../data/tweets_{0}.json'.format(username, limit))
+
+
+def get_screen_names_of_followers(user_ids, api):
     '''Returns a list of screen names for user given tweepy api'''
-    return [api.get_user(int(user_id)).screen_name for user_id in download_followers(user, api)]
+    return [api.get_user(int(user_id)).screen_name for user_id in user_ids]
 
 
 def get_followers_of_followers(user, api):
@@ -63,12 +82,11 @@ if __name__ == '__main__':
     #        user_followers[username] = get_followers_of_followers(username, main_api)
     #    with open('saved_followers.json', 'w+') as fl:
     #        json.dump(user_followers, fl)
-    which_to_scrape_lists = ['Sc1naps', 'SharCrook', 'MargaretCondeE', 'milthampton', 'lucabrivio', 'BartoszKochask1', 'neoosho', 'R3RT0', 'rogue_neuron', 'SliwinskaM', 'PetitetPierre', 'katjaQheuer', 'vinpetersen', 'msandstr', 'Ronald_vanLoon', 'lisa_schmors', 'dan_marinazzo', 'KrzysztofBiels1']
+#    which_to_scrape_lists = ['Sc1naps', 'SharCrook', 'MargaretCondeE', 'milthampton', 'lucabrivio', 'BartoszKochask1', 'neoosho', 'R3RT0', 'rogue_neuron', 'SliwinskaM', 'PetitetPierre', 'katjaQheuer', 'vinpetersen', 'msandstr', 'Ronald_vanLoon', 'lisa_schmors', 'dan_marinazzo', 'KrzysztofBiels1']
     user_followers = {}
-    for i_sublist, sublist in enumerate(which_to_scrape_lists):
-        user_followers = {}
-        for user in sublist:
-            user_followers[user] = download_followers(user, main_api)
-        with open('followers_part_{}.json'.format(i_sublist+1), 'w+') as fl:
-            json.dump(user_followers, fl)
+    user_ids = joblib.load('ids_brainhack.pkl')['ids_brainhack']
+    for user in user_ids:
+        user_followers[user] = download_followers_from_id(user, main_api)
+    with open('ids_brainhack.json', 'w+') as fl:
+        json.dump(user_followers, fl)
 
